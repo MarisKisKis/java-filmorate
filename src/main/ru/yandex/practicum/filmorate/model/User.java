68f0d3981b5644d9986e30f8fb7ba6c87d91d583
Logvinova <logvmarie@gmail.com>
2022-07-09 23:14:19 +0300

    8 sprint 1 version added

--- src/main/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/ru/yandex/practicum/filmorate/model/User.java
@@ -2,14 +2,16 @@ package yandex.practicum.filmorate.model;
 import lombok.Data;
 import lombok.NonNull;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Data
 public class User {
     private int id;
-    //@Email
+    @Email
     private String email;
-  //  @NotBlank
+    @NotBlank
     private String login;
     private String name;
     private LocalDate birthday;
