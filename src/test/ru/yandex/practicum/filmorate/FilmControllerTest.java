89a7df899df6f957dca157b2d1aac4e0729823d0
Logvinova <logvmarie@gmail.com>
2022-07-09 22:26:15 +0300

    8 sprint 1 version user name test added

--- src/test/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -6,8 +6,6 @@ import ru.yandex.practicum.filmorate.exception.FilmValidationException;
 import ru.yandex.practicum.filmorate.exception.UserValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.LocalDate;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FilmControllerTest {
@@ -26,10 +24,10 @@ public class FilmControllerTest {
         assertThrows(FilmValidationException.class, () -> filmController.validateFilm(film) );
     }
     @Test
-    void validateRelease() {
+    void validateDescription() {
         final FilmController filmController = new FilmController();
         final Film film = new Film();
-        film.setReleaseDate(LocalDate.of(1800, 1, 1));
+        film.setDescription("Военная драма о советском разведчике, внедрённом в высшие эшелоны власти нацистской Германии, снималась с 1971 по 1973 год. Показ фильма должен был начаться ко Дню Победы в мае 1973 года, но был отложен по политическим соображениям, из-за визита в эти дни советского лидера Леонида Брежнева в ФРГ. Первую серию картины зрители увидели 11 августа 1973 года. Фильм приобрёл широкую популярность в Советском Союзе уже во время премьерного показа, в связи с чем повторный показ состоялся уже через три месяца.");
         assertThrows(FilmValidationException.class, () -> filmController.validateFilm(film) );
     }
 }
