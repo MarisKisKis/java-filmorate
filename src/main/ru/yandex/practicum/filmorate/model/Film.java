82a84165aa4a0a1b11493ac9530f097dda3a2023
Logvinova <logvmarie@gmail.com>
2022-07-09 23:41:19 +0300

    annotation added

--- src/main/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,11 +3,13 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 import lombok.NonNull;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Data
 public class Film {
     private int id;
+    @NotBlank
     private String name;
     private String description;
     private LocalDate releaseDate;
