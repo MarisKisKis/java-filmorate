0d827ced5d58d41cdbb42b66985212959094d25b
Logvinova <logvmarie@gmail.com>
2022-07-10 23:17:33 +0300

    projecr refactored 3

--- src/test/filmorate/ru/yandex/practicum/FilmControllerTest.java
+++ src/test/filmorate/ru/yandex/practicum/FilmControllerTest.java
@@ -14,27 +14,27 @@ public class FilmControllerTest {
         final FilmController filmController = new FilmController();
         final Film film = new Film();
         film.setName(" ");
-        assertThrows(RuntimeException.class, () -> filmController.validateFilm(film) );
+        assertThrows(RuntimeException.class, () -> filmController.validate(film) );
     }
     @Test
     void validateDescription() {
         final FilmController filmController = new FilmController();
         final Film film = new Film();
         film.setDescription("Военная драма о советском разведчике, внедрённом в высшие эшелоны власти нацистской Германии, снималась с 1971 по 1973 год. Показ фильма должен был начаться ко Дню Победы в мае 1973 года, но был отложен по политическим соображениям, из-за визита в эти дни советского лидера Леонида Брежнева в ФРГ. Первую серию картины зрители увидели 11 августа 1973 года. Фильм приобрёл широкую популярность в Советском Союзе уже во время премьерного показа, в связи с чем повторный показ состоялся уже через три месяца.");
-        assertThrows(RuntimeException.class, () -> filmController.validateFilm(film) );
+        assertThrows(RuntimeException.class, () -> filmController.validate(film) );
     }
     @Test
     void validateRelease() {
         final FilmController filmController = new FilmController();
         final Film film = new Film();
         film.setReleaseDate(LocalDate.MAX);
-        assertThrows(RuntimeException.class, () -> filmController.validateFilm(film) );
+        assertThrows(RuntimeException.class, () -> filmController.validate(film) );
     }
     @Test
     void validateDuration() {
         final FilmController filmController = new FilmController();
         final Film film = new Film();
         film.setDuration(-100);
-        assertThrows(RuntimeException.class, () -> filmController.validateFilm(film) );
+        assertThrows(RuntimeException.class, () -> filmController.validate(film) );
     }
 }
