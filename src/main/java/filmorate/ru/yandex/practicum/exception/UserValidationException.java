94d713d3e763bbb0a7ca1cb60e19acee7a5cfa5e
Logvinova <logvmarie@gmail.com>
2022-07-16 21:15:51 +0300

    sprint 9 version 1.3

--- src/main/java/filmorate/ru/yandex/practicum/exception/UserValidationException.java
+++
@@ -1,7 +0,0 @@
-package filmorate.ru.yandex.practicum.exception;
-
-public class UserValidationException extends RuntimeException {
-    public UserValidationException(String message) {
-        super(message);
-    }
-}
